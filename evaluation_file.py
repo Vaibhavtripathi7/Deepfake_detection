c1e2eca81e6fea17cbc1b827319de9b9fe323424
Vaibhav Tripathi <vaibhavtripathi2020@gmail.com>
2025-06-01 01:42:33 +0530

    Changing parameters

--- evaluation_file.py
+++ evaluation_file.py
@@ -64,9 +64,9 @@ class VideoDataset(Dataset):
 
 class DeepFakeDetector(nn.Module):
     def __init__(self, num_classes=2, latent_dim=2048, lstm_layers=1,
-                 hidden_dim=256,  # Ensure this matches the trained model
+                 hidden_dim=64,  # Ensure this matches the trained model
                  bidirectional_lstm=False, # Ensure this matches the trained model
-                 dropout_rate=0.6):
+                 dropout_rate=0.8):
         super(DeepFakeDetector, self).__init__()
         cnn_model = models.resnext50_32x4d(weights=models.ResNeXt50_32X4D_Weights.IMAGENET1K_V2)
         self.cnn_features = nn.Sequential(*list(cnn_model.children())[:-2])
